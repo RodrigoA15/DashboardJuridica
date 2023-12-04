381e42ee0052bb05949877481e5d054757a4f50c
RodrigoA15 <rapapamija@misena.edu.co>
2023-12-04 14:58:45 -0500

    comandos despliegue1 Front

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL: 'http://192.168.28.74:4000/api',
-  baseURL: 'http://localhost:4000/api',
+  // baseURL: 'http://localhost:4000/api',
+  //Produccion
+  baseURL: 'https://pqrs-movit.onrender.com',
   withCredentials: true
 });
 
