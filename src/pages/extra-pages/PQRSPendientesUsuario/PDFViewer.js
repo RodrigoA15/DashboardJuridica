cbca591ca2ec6886a7ac2e82e3e5d245cf02a3de
RodrigoA15 <rapapamija@misena.edu.co>
2024-01-25 08:59:11 -0500

    Scroll PDF viewer

--- src/pages/extra-pages/PQRSPendientesUsuario/PDFViewer.js
+++ src/pages/extra-pages/PQRSPendientesUsuario/PDFViewer.js
@@ -1,20 +1,48 @@
 import { Viewer, Worker } from '@react-pdf-viewer/core';
 
 // Plugins
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+// import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 
 // Import styles
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import '@react-pdf-viewer/core/lib/styles/index.css';
 import PropTypes from 'prop-types';
 
 function PDFViewer({ url }) {
-  const defaultLayoutPluginInstance = defaultLayoutPlugin(defaultLayoutPlugin);
+  // const defaultLayoutPluginInstance = defaultLayoutPlugin(defaultLayoutPlugin);
 
   return (
     <div>
       <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
-        <Viewer fileUrl={url} plugins={[defaultLayoutPluginInstance]} />
+        <div>
+          <div
+            style={{
+              border: '1px solid rgba(0, 0, 0, 0.3)',
+              height: '450px',
+              width: '100%',
+              margin: '0 auto',
+              boxSizing: 'border-box'
+            }}
+          >
+            {url ? (
+              <Viewer fileUrl={url} />
+            ) : (
+              <div
+                style={{
+                  border: '2px dashed rgba(0, 0, 0, .3)',
+                  fontSize: '2rem',
+                  height: '100%',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center'
+                }}
+              >
+                Preview area
+              </div>
+            )}
+          </div>
+        </div>
       </Worker>
     </div>
   );
