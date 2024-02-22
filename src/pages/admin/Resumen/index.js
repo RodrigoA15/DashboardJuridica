27eb508a9839d23971a8fd17bea286b6e27a3f9a
RodrigoA15 <rapapamija@misena.edu.co>
2024-02-22 11:42:32 -0500

    Modificacion diseño graficas resumen admin

--- src/pages/admin/Resumen/index.js
+++ src/pages/admin/Resumen/index.js
@@ -7,18 +7,6 @@ import ChartEstados from './chartEstados';
 function IndexResumen() {
   return (
     <Grid container rowSpacing={4.5} columnSpacing={2.75}>
-      {/* Entidades */}
-      <Grid item xs={8} md={5}>
-        <Grid container alignItems="center" justifyContent="space-between">
-          <Grid item>
-            <Typography variant="h5">Resumen</Typography>
-          </Grid>
-        </Grid>
-        <MainCard content={false} sx={{ mt: 1.5 }}>
-          <ChartEntidadAdmin />
-        </MainCard>
-      </Grid>
-
       <Grid item xs={8} md={5}>
         <Grid container alignItems="center" justifyContent="space-between">
           <Grid item>
@@ -38,6 +26,18 @@ function IndexResumen() {
         </Grid>
         <MainCard content={false} sx={{ mt: 1.5 }}>
           <ChartEstados />
+        </MainCard>
+      </Grid>
+
+      {/* Entidades */}
+      <Grid item xs={8} md={5}>
+        <Grid container alignItems="center" justifyContent="space-between">
+          <Grid item>
+            <Typography variant="h5">Resumen</Typography>
+          </Grid>
+        </Grid>
+        <MainCard content={false} sx={{ mt: 1.5 }}>
+          <ChartEntidadAdmin />
         </MainCard>
       </Grid>
     </Grid>
