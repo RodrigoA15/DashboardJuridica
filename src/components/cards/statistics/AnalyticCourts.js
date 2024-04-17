2d8be8a0f4eac41c264284500f53a3710445b25b
RodrigoA15 <rapapamija@misena.edu.co>
2024-04-17 15:12:37 -0500

    Card entidades juridicas

--- src/components/cards/statistics/AnalyticCourts.js
+++ src/components/cards/statistics/AnalyticCourts.js
@@ -3,7 +3,7 @@ import axios from 'api/axios';
 import { Toaster, toast } from 'sonner';
 import MainCard from 'components/MainCard';
 import { Grid, Stack, Typography } from '@mui/material';
-
+import { meses } from 'data/meses';
 function AnalyticCourts() {
   const [courtData, setCourtData] = useState([]);
 
@@ -23,6 +23,8 @@ function AnalyticCourts() {
       }
     }
   };
+
+  const mesActual = new Date();
   return (
     <div>
       <Toaster position="top-right" richColors expand={true} offset="80px" />
@@ -30,12 +32,12 @@ function AnalyticCourts() {
       <MainCard contentSX={{ p: 2.25 }} className="cardEntidad">
         <Stack spacing={0.5}>
           <Typography style={{ margin: 'auto' }} variant="h6" color="textSecondary">
-            PQRS Entidades Juridicas
+            PQRS Entidades Juridicas - {meses[mesActual.getMonth()]}
           </Typography>
           <Grid container alignItems="center">
             <Grid item style={{ margin: 'auto' }}>
               <Typography variant="h3" color="inherit">
-                {courtData.map((count) => count.total)}
+                {courtData.map((count) => count.count)}
               </Typography>
             </Grid>
           </Grid>
