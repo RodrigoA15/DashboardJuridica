ba5c1e3af18140d20f88f968d3ad6e49b61e11e4
RodrigoA15 <rapapamija@misena.edu.co>
2024-06-20 07:53:45 -0500

    Add: dates festive

--- src/hooks/useDate.js
+++ src/hooks/useDate.js
@@ -1,7 +1,20 @@
 const useDiasHabiles = () => {
   const diasHabiles = (fecha_radicado) => {
     let contador = 0;
-    let festivos = ['2024-03-25', '2024-03-28', '2024-03-29'];
+    let festivos = [
+      '2024-03-25',
+      '2024-03-28',
+      '2024-03-29',
+      '2024-06-03',
+      '2024-06-10',
+      '2024-07-01',
+      '2024-07-20',
+      '2024-08-19',
+      '2024-10-14',
+      '2024-11-04',
+      '2024-12-08',
+      '2024-12-25'
+    ];
     let fechaInicio = new Date(fecha_radicado);
     let fechaCalculo = new Date(fechaInicio);
     fechaCalculo.setDate(fechaCalculo.getDate() + 1);
