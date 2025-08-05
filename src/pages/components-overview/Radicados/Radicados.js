4791e1758b30794af8104b7b4d6fef5aade699ce
RodrigoA15 <rapapamija@misena.edu.co>
2025-08-05 09:00:08 -0500

    Add: Form provider

--- src/pages/components-overview/Radicados/Radicados.js
+++ src/pages/components-overview/Radicados/Radicados.js
@@ -11,17 +11,19 @@ import { Button } from '@mui/material';
 import { useAuth } from 'context/authContext';
 import ModalComponent from './ImportFile/modal';
 import { Parameters } from 'hooks/useParameters';
+import { FormProvider } from 'react-hook-form';
 function ComponentRadicados() {
+  const methods = useForm({ mode: 'onChange' });
   const {
     register,
     handleSubmit,
     formState: { errors },
     reset,
     watch
-  } = useForm({ mode: 'onChange' });
+  } = methods;
+
   const { user } = useAuth();
   const [procedencia, setProcedencia] = useState('');
-  const [id_departamento, setIdDepartamento] = useState('');
   const [id_tipificacion, setIdTipificacion] = useState('');
   const [nameCourt, setNameCourt] = useState('');
   const [juzgado, setJuzgados] = useState(null);
@@ -37,10 +39,10 @@ function ComponentRadicados() {
         ...data,
         id_procedencia: procedencia,
         estado_radicado: 'Pre-asignacion',
-        id_departamento,
         id_tipificacion,
         juzgado
       };
+
       await axios.post(`/radicados`, datos);
       MySwal.fire({
         title: 'Creado correctamente',
@@ -72,122 +74,124 @@ function ComponentRadicados() {
             setJuzgados={setJuzgados}
           />
           <hr />
-          <form onSubmit={onSubmit}>
-            {/* Radicados */}
-            <div className="row mb-3">
-              <h4>Informaci&oacute;n radicado</h4>
-              <div className="mb-3 col">
-                <label htmlFor="numero" className="form-label h6">
-                  N&uacute;mero radicado*
-                </label>
-                <input
-                  aria-label="radicados"
-                  type="number"
-                  className="form-control rounded-pill minimal-input-dark"
-                  id="radicados"
-                  {...register('numero_radicado', {
-                    required: {
-                      value: true,
-                      message: 'Número radicado es obligatorio'
-                    },
-                    minLength: {
-                      value: 14,
-                      message: 'Número Radicado debe ser mayor igual a 14 caracteres'
-                    },
+          <FormProvider {...methods}>
+            <form onSubmit={onSubmit}>
+              {/* Radicados */}
+              <div className="row mb-3">
+                <h4>Informaci&oacute;n radicado</h4>
+                <div className="mb-3 col">
+                  <label htmlFor="numero" className="form-label h6">
+                    N&uacute;mero radicado*
+                  </label>
+                  <input
+                    aria-label="radicados"
+                    type="number"
+                    className="form-control rounded-pill minimal-input-dark"
+                    id="radicados"
+                    {...register('numero_radicado', {
+                      required: {
+                        value: true,
+                        message: 'Número radicado es obligatorio'
+                      },
+                      minLength: {
+                        value: 14,
+                        message: 'Número Radicado debe ser mayor igual a 14 caracteres'
+                      },
 
-                    maxLength: {
-                      value: 18,
-                      message: 'Número Radicado no debe sobrepasar 18 caracteres'
-                    }
-                  })}
-                />
-                {errors.numero_radicado && <span className="inputForm ">{errors.numero_radicado.message}</span>}
-              </div>
-
-              <div className="mb-3 col">
-                <label htmlFor="fecha" className="form-label h6">
-                  Fecha radicado*
-                </label>
-                <input
-                  aria-label="Search"
-                  type="date"
-                  className="form-control rounded-pill minimal-input-dark"
-                  id="fecha"
-                  {...register('fecha_radicado', {
-                    required: {
-                      value: true,
-                      message: 'Fecha radicado es obligatorio'
-                    },
-                    valueAsDate: true,
-
-                    validate: (value) => {
-                      const fechaRadicado = new Date(value);
-                      const fechaActual = new Date();
-                      // Obtener la diferencia en milisegundos entre las dos fechas
-                      const diferenciaTiempo = fechaActual - fechaRadicado;
-                      // Convertir la diferencia a meses
-                      const diferenciaMeses = diferenciaTiempo / (1000 * 60 * 60 * 24 * 30);
-
-                      if (diferenciaMeses > 6) {
-                        return 'Fecha inválida, las fechas de radicación no pueden ser mayores a 6 meses';
-                      } else if (fechaRadicado > fechaActual) {
-                        return 'Fecha inválida, la fecha de radicación no puede ser mayor a la fecha actual';
+                      maxLength: {
+                        value: 18,
+                        message: 'Número Radicado no debe sobrepasar 18 caracteres'
                       }
-                      // Retorna undefined si la fecha es válida
-                      return undefined;
-                    }
-                  })}
-                />
-                {errors.fecha_radicado && <span className="inputForm ">{errors.fecha_radicado.message}</span>}
-              </div>
-            </div>
+                    })}
+                  />
+                  {errors.numero_radicado && <span className="inputForm ">{errors.numero_radicado.message}</span>}
+                </div>
 
-            <div>
-              <div className="mb-3">
-                <GetEntrada register={register} errors={errors} />
+                <div className="mb-3 col">
+                  <label htmlFor="fecha" className="form-label h6">
+                    Fecha radicado*
+                  </label>
+                  <input
+                    aria-label="Search"
+                    type="date"
+                    className="form-control rounded-pill minimal-input-dark"
+                    id="fecha"
+                    {...register('fecha_radicado', {
+                      required: {
+                        value: true,
+                        message: 'Fecha radicado es obligatorio'
+                      },
+                      valueAsDate: true,
+
+                      validate: (value) => {
+                        const fechaRadicado = new Date(value);
+                        const fechaActual = new Date();
+                        // Obtener la diferencia en milisegundos entre las dos fechas
+                        const diferenciaTiempo = fechaActual - fechaRadicado;
+                        // Convertir la diferencia a meses
+                        const diferenciaMeses = diferenciaTiempo / (1000 * 60 * 60 * 24 * 30);
+
+                        if (diferenciaMeses > 6) {
+                          return 'Fecha inválida, las fechas de radicación no pueden ser mayores a 6 meses';
+                        } else if (fechaRadicado > fechaActual) {
+                          return 'Fecha inválida, la fecha de radicación no puede ser mayor a la fecha actual';
+                        }
+                        // Retorna undefined si la fecha es válida
+                        return undefined;
+                      }
+                    })}
+                  />
+                  {errors.fecha_radicado && <span className="inputForm ">{errors.fecha_radicado.message}</span>}
+                </div>
               </div>
 
               <div>
-                <GetEntidad register={register} errors={errors} setIdDepartamento={setIdDepartamento} id_departamento={id_departamento} />
-              </div>
-            </div>
-            <div className="row justify-content-center align-items-center">
-              <div className="col-2">
-                <Button variant="text" className="btn-block" onClick={() => setCheck((prevCheck) => !prevCheck)}>
-                  {check ? 'Cancelar Observación' : 'Agregar Observación'}
-                </Button>
-              </div>
-              {check && (
-                <div className="col-4">
-                  <div className="form-group">
-                    <label htmlFor="observaciones">Observaciones*</label>
-                    <textarea
-                      className="form-control"
-                      rows="3"
-                      placeholder="Observaciones Radicado"
-                      disabled={!check}
-                      {...register('observaciones_radicado')}
-                    ></textarea>
-                  </div>
+                <div className="mb-3">
+                  <GetEntrada register={register} errors={errors} />
                 </div>
-              )}
-              <div className="col-3">
-                <button type="submit" className="btn btn-success btn-block" disabled={shouldDisableButton}>
-                  Registrar
-                </button>
-              </div>
 
-              {parameters.map(
-                (parametro) =>
-                  parametro.nombre_parametro === 'Importar archivo' &&
-                  parametro.activo === true && (
-                    <div className="col-3" key={parametro._id}>
-                      <ModalComponent />
+                <div>
+                  <GetEntidad register={register} errors={errors} watch={watch} />
+                </div>
+              </div>
+              <div className="row justify-content-center align-items-center">
+                <div className="col-2">
+                  <Button variant="text" className="btn-block" onClick={() => setCheck((prevCheck) => !prevCheck)}>
+                    {check ? 'Cancelar Observación' : 'Agregar Observación'}
+                  </Button>
+                </div>
+                {check && (
+                  <div className="col-4">
+                    <div className="form-group">
+                      <label htmlFor="observaciones">Observaciones*</label>
+                      <textarea
+                        className="form-control"
+                        rows="3"
+                        placeholder="Observaciones Radicado"
+                        disabled={!check}
+                        {...register('observaciones_radicado')}
+                      ></textarea>
                     </div>
-                  )
-              )}
-            </div>
-          </form>
+                  </div>
+                )}
+                <div className="col-3">
+                  <button type="submit" className="btn btn-success btn-block" disabled={shouldDisableButton}>
+                    Registrar
+                  </button>
+                </div>
+
+                {parameters.map(
+                  (parametro) =>
+                    parametro.nombre_parametro === 'Importar archivo' &&
+                    parametro.activo === true && (
+                      <div className="col-3" key={parametro._id}>
+                        <ModalComponent />
+                      </div>
+                    )
+                )}
+              </div>
+            </form>
+          </FormProvider>
 
           <GetTipificacion setIdTipificacion={setIdTipificacion} />
         </div>
