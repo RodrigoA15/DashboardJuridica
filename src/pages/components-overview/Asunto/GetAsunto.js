128b3956d2c5dfae21699b6c5b6ee67272494556
RodrigoA15 <rapapamija@misena.edu.co>
2025-08-05 09:08:54 -0500

    Fix: Get affairs list

--- src/pages/components-overview/Asunto/GetAsunto.js
+++ src/pages/components-overview/Asunto/GetAsunto.js
@@ -1,49 +1,58 @@
 import { useEffect, useState } from 'react';
-import axios from '../../../api/axios';
-import PropTypes from 'prop-types';
+import { useFormContext } from 'react-hook-form';
+import axios from 'api/axios';
 
-function GetAsunto({ register, id_departamento, errors }) {
+function GetAsunto() {
   const [dataAsunto, setDataAsunto] = useState([]);
+  const {
+    register,
+    watch,
+    formState: { errors }
+  } = useFormContext();
+  const id_departamento = watch('id_departamento');
 
   useEffect(() => {
     if (id_departamento) {
-      listaAsuntos();
+      listaAsuntos(id_departamento);
+    } else {
+      setDataAsunto([]);
     }
   }, [id_departamento]);
 
-  const listaAsuntos = async () => {
+  const listaAsuntos = async (areaId) => {
     try {
-      const response = await axios.get(`/affair/asuntos_departamento/${id_departamento}`);
+      const response = await axios.get(`/affair/asuntos_departamento/${areaId}`);
       setDataAsunto(response.data);
     } catch (error) {
-      console.log(error);
+      console.error('Error al obtener asuntos:', error);
     }
   };
 
   return (
     <div>
+      <label htmlFor="id_asunto" className="form-label">
+        Asunto
+      </label>
       <select
         className="form-select rounded-pill minimal-input-dark"
-        {...register('id_asunto', { required: 'Campo asunto es obligatorio' })}
+        {...register('id_asunto', {
+          required: 'Campo asunto es obligatorio'
+        })}
       >
         <option value="">Seleccione un asunto</option>
-        {dataAsunto && dataAsunto.length > 0 ? (
+        {dataAsunto.length > 0 ? (
           dataAsunto.map((i) => (
             <option key={i._id} value={i._id}>
               {i.nombre_asunto}
             </option>
           ))
         ) : (
-          <option disabled>No asuntos</option>
+          <option disabled>No hay asuntos</option>
         )}
       </select>
-      {errors.id_asunto && <span className="inputForm">{errors.id_asunto.message}</span>}
+      {errors?.id_asunto && <span className="inputForm">{errors.id_asunto.message}</span>}
     </div>
   );
 }
 
 export default GetAsunto;
-
-GetAsunto.propTypes = {
-  register: PropTypes.func
-};
