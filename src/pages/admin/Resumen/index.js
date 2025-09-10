fe295f4e1ed1356120d9ed5c9fadb53c07bf443e
RodrigoA15 <rapapamija@misena.edu.co>
2025-09-10 12:45:47 -0500

    Add: Chart Percentage PQRD in admin module

--- src/pages/admin/Resumen/index.js
+++ src/pages/admin/Resumen/index.js
@@ -4,10 +4,21 @@ import ChartArea from './chartArea';
 import { ChartEstados } from './chartEstados';
 import { BarChart } from './StatesByUser';
 import { TableUsersAnswer } from './tableUsersAnswer';
+import ChartRadicadosAnswer from 'pages/dashboard/ChartRadicadosAnswer';
+import { Parameters } from 'hooks/useParameters';
+import { useEffect, useState } from 'react';
 // import { ChartPQRSmonth } from './chartPQRSmonth';
 // import { AnswerByuser } from './answerByuser';
 
 function IndexResumen() {
+  const { parameters } = Parameters();
+  const [validateParam, setValidateParam] = useState(false);
+  useEffect(() => {
+    const validatorParameter = parameters.some((parametro) => parametro.nombre_parametro === 'Tabla asuntos' && parametro.activo === true);
+    if (validatorParameter !== validateParam) {
+      setValidateParam(validatorParameter);
+    }
+  }, [parameters, validateParam]);
   return (
     <Grid container rowSpacing={4.5} columnSpacing={2.75}>
       <Grid item xs={6} md={6}>
@@ -42,6 +53,19 @@ function IndexResumen() {
           <TableUsersAnswer />
         </MainCard>
       </Grid>
+
+      {validateParam && (
+        <Grid item xs={6} md={6}>
+          <Grid container alignItems="center" justifyContent="space-between">
+            <Grid item>
+              <Typography variant="h5">Total porcentaje de PQRS sin responder</Typography>
+            </Grid>
+          </Grid>
+          <MainCard content={false} sx={{ mt: 1.5 }}>
+            <ChartRadicadosAnswer />
+          </MainCard>
+        </Grid>
+      )}
 
       <Grid item xs={6} md={12}>
         <Grid container alignItems="center" justifyContent="space-between">
