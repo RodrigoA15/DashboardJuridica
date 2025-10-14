38daade3b078fdaccaac691ebff0102c7aae8fd6
RodrigoA15 <rapapamija@misena.edu.co>
2025-10-14 11:03:23 -0500

    Add: Prime react styles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import ScrollTop from 'components/ScrollTop';
 import { useEffect } from 'react';
 import { useAuth } from 'context/authContext';
 import LoaderComponent from 'components/LoaderComponent';
+//Primereact styles
+import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
+import 'primereact/resources/primereact.min.css'; //core css
 
 // ==============================|| APP - THEME, ROUTER, LOCAL  ||============================== //
 
