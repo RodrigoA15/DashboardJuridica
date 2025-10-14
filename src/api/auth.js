dbf3c3754b029ddd10a81ee3636b384278a11e47
RodrigoA15 <rapapamija@misena.edu.co>
2025-10-14 14:36:08 -0500

    Fix: Return data axios auth

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,18 @@
 import axios from './axios';
 
-export const register = (user) => axios.post(`/auth/register`, user);
+// Registro de usuario
+export const register = async (user) => {
+  const { data } = await axios.post('/auth/register', user);
+  return data;
+};
 
-export const login = (user) => axios.post(`/auth/login`, user);
+// Login de usuario
+export const login = async (user) => {
+  const { data } = await axios.post('/auth/login', user);
+  return data;
+};
 
-export const verifyToken = () => axios.get('/auth/verify');
+export const verifyToken = async () => {
+  const { data } = await axios.get('/auth/verify');
+  return data;
+};
