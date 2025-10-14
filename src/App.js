2197472b61758077dc6c48b7e852d59274d88b99
RodrigoA15 <rapapamija@misena.edu.co>
2025-10-14 14:33:51 -0500

    Fix: Load User verify

--- src/App.js
+++ src/App.js
@@ -2,26 +2,19 @@
 import { ThemeRoutes } from './routes/index';
 import ThemeCustomization from 'themes';
 import ScrollTop from 'components/ScrollTop';
-import { useEffect } from 'react';
-import { useAuth } from 'context/authContext';
-import LoaderComponent from 'components/LoaderComponent';
 //Primereact styles
 import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
 import 'primereact/resources/primereact.min.css'; //core css
+import LoaderComponent from 'components/LoaderComponent';
+import { useAuth } from 'context/authContext';
 
 // ==============================|| APP - THEME, ROUTER, LOCAL  ||============================== //
 
 const App = () => {
-  const { checkLogin, loading } = useAuth();
-
-  useEffect(() => {
-    checkLogin();
-  }, []);
-
-  if (loading) {
-    return <LoaderComponent />; // Mostrar el loader mientras loading es true
+  const { isLoading } = useAuth();
+  if (isLoading) {
+    return <LoaderComponent />;
   }
-
   return (
     <ThemeCustomization>
       <ScrollTop>
