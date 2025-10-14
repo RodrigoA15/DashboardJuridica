47dccda9e89d1cc4a7d8d551a34c111b9a24ddb6
RodrigoA15 <rapapamija@misena.edu.co>
2025-10-14 11:00:01 -0500

    Fix: Select List areas

--- src/pages/extra-pages/PQRSPendientesUsuario/ActualizarArea/ListAreas.js
+++ src/pages/extra-pages/PQRSPendientesUsuario/ActualizarArea/ListAreas.js
@@ -1,30 +1,31 @@
 import { memo } from 'react';
+import { Dropdown } from 'primereact/dropdown';
 import { ApiAreas } from './ApiAreas';
 import PropTypes from 'prop-types';
-export const ListAreas = memo(({ setNameArea, granted }) => {
+export const ListAreas = memo(({ nameArea, setNameArea, granted }) => {
   const { areas } = ApiAreas();
-
-  const handleSubmit = (e) => {
-    setNameArea(e.target.value);
-  };
 
   return (
     <>
-      <select className="form-select rounded-pill mb-3" onChange={handleSubmit} disabled={granted === 'Devuelto'}>
-        <option value="">Seleccione &aacute;rea</option>
-        {areas
-          .filter((name) => name.nombre_departamento != 'Juridica - MDA')
-          .map((area) => (
-            <option key={area._id} value={area._id}>
-              {area.nombre_departamento}
-            </option>
-          ))}
-      </select>
+      <div className="flex flex-col gap-2 md:col-span-2">
+        <label className="text-sm font-medium text-gray-700" htmlFor="areas">
+          &Aacute;rea
+        </label>
+        <Dropdown
+          value={nameArea}
+          options={areas}
+          onChange={(e) => setNameArea(e.value)}
+          disabled={granted === 'Devuelto'}
+          optionLabel="nombre_departamento"
+          placeholder="Seleccione área"
+        />
+      </div>
     </>
   );
 });
 
 ListAreas.propTypes = {
+  nameArea: PropTypes.object,
   setNameArea: PropTypes.func.isRequired,
   granted: PropTypes.string
 };
