aeccb1622de9dea90283574817534f796fed3b9c
RodrigoA15 <rapapamija@misena.edu.co>
2026-01-08 16:55:21 -0500

    Add: Validation returned data

--- src/pages/dashboard/Metrics/MetricTotalEntities.js
+++ src/pages/dashboard/Metrics/MetricTotalEntities.js
@@ -18,8 +18,8 @@ export const MetricTotalEntities = () => {
       {data?.map((item) => (
         <div key={item.entidad} className="rounded-2xl border border-gray-200 bg-[#5A96E3] p-4 h-24">
           <div className="flex flex-col items-center justify-center text-center">
-            <p className="text-sm text-white/90">{item.entidad}</p>
-            <h4 className="text-2xl font-bold text-white/90">{item.count}</h4>
+            <p className="text-sm text-white/90">{item.entidad || 'N/A'}</p>
+            <h4 className="text-2xl font-bold text-white/90">{item.count || 0}</h4>
           </div>
         </div>
       ))}
