5ef5a243d3bf6a2fe4245581d4272b3723cec3bc
RodrigoA15 <rodri150306@gmail.com>
2026-05-28 11:40:35 -0500

    New: Copyrigth Quipux Popayan

--- src/layout/MainLayout/Drawer/DrawerContent/index.js
+++ src/layout/MainLayout/Drawer/DrawerContent/index.js
@@ -1,4 +1,5 @@
-// project import
+// material-ui
+import { Box } from '@mui/material';
 
 import Navigation from './Navigation';
 import SimpleBar from 'components/third-party/SimpleBar';
@@ -6,16 +7,25 @@ import SimpleBar from 'components/third-party/SimpleBar';
 // ==============================|| DRAWER CONTENT ||============================== //
 
 const DrawerContent = () => (
-  <SimpleBar
-    sx={{
-      '& .simplebar-content': {
-        display: 'flex',
-        flexDirection: 'column'
-      }
-    }}
-  >
-    <Navigation />
-  </SimpleBar>
+  <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+    <Box sx={{ flex: 1, minHeight: 0 }}>
+      <SimpleBar
+        sx={{
+          '& .simplebar-content': {
+            display: 'flex',
+            flexDirection: 'column'
+          }
+        }}
+      >
+        <Navigation />
+      </SimpleBar>
+    </Box>
+
+    <Box sx={{ mt: 'auto', p: 2, textAlign: 'center' }}>
+      <p className='font-semibold text-xs'>Powered by Quipux Popay&aacute;n</p>
+      <p className='text-xs'>&copy; {new Date().getFullYear()}</p>
+    </Box>
+  </Box>
 );
 
 export default DrawerContent;
